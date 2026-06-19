019d47afd242fa4830d6fb20776ed04e9ad671e6
cjjimenez-dev <jimenezchristianjohngran07@gmail.com>
2026-06-20 02:05:51 +0800

    Allow CORS requests from Vercel domain

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5175"
+    origins "http://localhost:5175", "https://shift-dox.vercel.app"
 
     resource "*",
       headers: :any,
